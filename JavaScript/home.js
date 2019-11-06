602fcf46c09465e464757e3423cb4564b9b45650
Justin Plein <justin.plein1@student.rocva.nl>
2019-11-06 11:29:15 +0100

    kleine updtaes toegevoegd en nieuws verwijdert.

---
+++ JavaScript/home.js
@@ -0,0 +1,17 @@
+function buttonE() {
+    document.getElementById("button1").style.backgroundColor = "green";;
+    document.getElementById("button1").style.width = "100px";
+    document.getElementById("button1").style.height = "100px";
+    document.getElementById("button2").style.width = "50px";
+    document.getElementById("button2").style.height = "50px";
+    document.getElementById("button2").backgroundColor = "grey";
+
+}
+function buttonT() {
+    document.getElementById("button2").style.backgroundColor = "red";
+    document.getElementById("button2").style.width = "100px";
+    document.getElementById("button2").style.height = "100px";
+    document.getElementById("button1").style.width = "50px";
+    document.getElementById("button1").style.height = "50px";
+    document.getElementById("button1").backgroundColor = "gray";
+}
